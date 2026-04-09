39492528d1d2359d91973ec3cf6f61b4eee64a75
Casi <19928431+CassandraNewell@users.noreply.github.com>
2026-04-09 16:07:16 +0200

    fix noisy terraform

--- infra/cloudrun.tf
+++ infra/cloudrun.tf
@@ -92,6 +92,12 @@ resource "google_cloud_run_v2_service" "api" {
     }
   }
 
+  lifecycle {
+    ignore_changes = [
+      template[0].containers[0].image,
+    ]
+  }
+
   depends_on = [
     google_project_service.cloud_run,
     google_secret_manager_secret_version.database_url,
